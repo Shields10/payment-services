0f2846673dc8093273f8bbee292fd3115a461889
Shields10 <benjaminshields10@gmail.com>
2024-06-10 18:32:35 +0300

    Changed MpesaStkPushApplication to PaymentServiceApplication
    Signed-off-by: Shields10 <benjaminshields10@gmail.com>

--- src/test/java/com/benji/payments_services/PaymentServiceApplicationTests.java
+++ src/test/java/com/benji/payments_services/PaymentServiceApplicationTests.java
@@ -3,8 +3,6 @@ package com.benji.payments_services;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ContextConfiguration;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 @ContextConfiguration(classes = PaymentServiceApplication.class)
 class PaymentServiceApplicationTests {
 	@Test
